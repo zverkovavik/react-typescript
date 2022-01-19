85963f3ac8a206faf28cc5a1d666d491f9d34d04
Viktorya <52043307+zverkovavik@users.noreply.github.com>
2022-01-19 16:56:00 +0300

    Typing portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
+
+type PortalProps = {
+  children: React.ReactNode,
+}
+
+class Portal extends Component<PortalProps> {
+  private el: HTMLDivElement = document.createElement('div');
+  public componentDidMount():void {
+    document.body.appendChild(this.el);
+  }
+  public componentWillUnmount():void {
+    document.body.removeChild(this.el);
+  }
+  public render(): React.ReactElement<PortalProps> {
+    return ReactDOM.createPortal(this.props.children, this.el);
+  }
+}
+class MyComponent extends Component<{}, { count: number }> {
+  state = {
+    count: 0,
+  }
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: ++count,
+    }));
+  }
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        <h1>Clicks: {this.state.count}</h1>
+        <Portal>
+          <h2>TEST PORTAL</h2>
+          <button>Click</button>
+        </Portal>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyComponent />
   );
 }
 
