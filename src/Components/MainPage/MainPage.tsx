499b6677acc42a2b152b33901652b70619e1caa8
Viktorya Zverkova <viktorya_zverkova@mail.ru>
2022-02-10 23:25:15 +0300

    Created cover
    Added fonts, added react-router-dom v6, added cover and Navigation Panel

---
+++ src/Components/MainPage/MainPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import NavigationPanel from '../NavigationPanel/NavigationPanel';
+
+function MainPage() {
+    return (
+        <>
+            <NavigationPanel />
+            <main>
+                <section className="promo">
+                    <h1 className="promo__title">Digital Planet VR</h1>
+                    <p className="promo__description">Образовательный виртуальный планетарий в Новороссийске.
+                        Познавательное приключение в мир звёзд и галактик.</p>
+                </section>
+                <section>
+                    <h2>Что такое VR-планетарий?</h2>
+                    <p className="text-description">Это образовательная программа, использующая технологию виртуальной реальности. <br />
+                        Зрителям выдаются очки виртуальной реальности, оператор запускает сеанс планетария на всех очках одновременно. В коллекции фильмов идут обучающие картины о космосе, астрономии  и обществознании. <br />
+                        За счёт реалестичности  и имитации игры такое обучение повышает мотивацию и стимулирует активную мозговую деятельность ребёнка. </p>
+                </section>
+            </main>
+        </>
+    );
+};
+
+export default MainPage;
---
+++ src/Components/NavigationPanel/NavigationPanel.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function NavigationPanel() {
+    return (
+    <header className="main-header">
+        <div className="logo">
+            <img src="./img/logo.png" alt="" className="logo__picture" />
+        </div>
+        <nav className="nav-menu">
+            <ul className="nav-menu__list">
+                <li className="nav-menu__item">О продукте</li>
+                <li className="nav-menu__item">Как работает</li>
+                <li className="nav-menu__item">Технология</li>
+                <li className="nav-menu__item">Вопросы</li>
+                <li className="nav-menu__item">Контакты</li>
+            </ul>
+        </nav>
+        <div className="get-ticket">
+            <button className="get-ticket__button">
+                Заказать
+            </button>
+            <a href="tel:+88005553575">+ 8 (800)555 35 75</a>
+        </div>
+    </header>
+    );
+};
+
+export default NavigationPanel;
